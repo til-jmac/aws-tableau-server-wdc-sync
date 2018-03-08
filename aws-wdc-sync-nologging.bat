444c1cde9271c566c5e675068e122b41de967042
Jmac <jonathan.macdonald@theinformationlab.co.uk>
2018-03-08 19:04:49 +0000

    initial add

--- aws-wdc-sync-nologging.bat
+++ aws-wdc-sync-nologging.bat
@@ -2,6 +2,6 @@
 
 echo %DATE% %TIME% Starting AWS S3 WDC sync 
 
-aws s3 sync --only-show-errors --delete s3://eif4smes-test-wdc "D:\Program Files\Tableau\Tableau Server\data\tabsvc\httpd\htdocs\webdataconnectors" 
+aws s3 sync --only-show-errors --delete s3://test-wdc "D:\Program Files\Tableau\Tableau Server\data\tabsvc\httpd\htdocs\webdataconnectors" 
  
 echo %DATE% %TIME% AWS S3 WDC sync complete 
